2e9fbf94c9a12f4ed905adc690c182e74fcb1076
Kuni <kuni.ujiie@mescius.com>
2024-08-06 17:33:49 +0900

    プロジェクト ファイルを追加します。

---
+++ DioDocsExcelSort/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GrapeCity.Documents.Excel;
+
+Console.WriteLine("DioDocs for Excelのソート機能");
+
+// 新しいワークブックを作成します
+Workbook workbook = new Workbook();
+
+// Excelファイルを開きます
+workbook.Open("test.xlsx");
+IWorksheet worksheet = workbook.Worksheets[0];
+
+//// C列の値でソートします
+worksheet.Range["A2:F21"].Sort(worksheet.Range["C2:C21"],
+                               orientation: SortOrientation.Columns);
+
+// 複数列（C列、D列）の値でソートします
+worksheet.Range["A2:F21"].Sort(
+    SortOrientation.Columns, 
+    false,
+    new ValueSortField[] {
+        new(worksheet.Range["C2:C21"], SortOrder.Ascending),
+        new(worksheet.Range["F2:F21"], SortOrder.Descending)
+    });
+
+// ワークブックをExcelファイルとして保存します
+workbook.Save("result.xlsx");
